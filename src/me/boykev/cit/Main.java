4e8e51414e1ce2c1ea98b994e87eb8e66befa0c0
JustBoyke <mail@boykevanvugt.nl>
2021-11-17 21:22:55 +0100

    Added plugin commands

--- src/me/boykev/cit/Main.java
+++ src/me/boykev/cit/Main.java
@@ -18,11 +18,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import de.tr7zw.nbtapi.NBTItem;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 
 public class Main extends JavaPlugin{
 	
@@ -206,6 +209,27 @@ public class Main extends JavaPlugin{
 				it.setString("mtcustom", args[1]);
 				p.getInventory().addItem(it.getItem());
 			}
+		}
+		if(cmd.getName().equalsIgnoreCase("customitems")) {
+			if(args.length > 0) {
+				if(args[0].equalsIgnoreCase("pling")) {
+					p.playSound(p.getLocation(), Sound.BLOCK_NOTE_HARP, 100F, 0.3F);
+					return false;
+				}
+			}
+			PluginDescriptionFile pdf = this.getDescription();
+			
+	        TextComponent component = new TextComponent(TextComponent.fromLegacyText(ChatColor.GREEN + "Developer: Boyke (boykev)"));
+	        component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://twitch.tv/gewoonboyke"));
+	        
+	        TextComponent version = new TextComponent(TextComponent.fromLegacyText(ChatColor.GREEN + "Versie: " + pdf.getVersion()));
+	        version.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/customitems pling"));
+			
+			p.sendMessage(ChatColor.GRAY + "---- [ Custom Items ] ----");
+			p.spigot().sendMessage(version);
+			p.spigot().sendMessage(component);
+			p.sendMessage(ChatColor.GREEN + "Website: https://boykevanvugt.nl");
+			p.sendMessage(ChatColor.GRAY + "----------------------");
 		}
 		return false;
 	}
